9adcd84cee0959fd0e7486b551e726a9fe6ee603
michaellearncode <michael.nuyennuyen@gmail.com>
2025-10-02 13:32:25 +0700

    init: create flutter app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [Text('Hi')],
+          ),
+        ),
+      ),
+    ),
+  );
+}
